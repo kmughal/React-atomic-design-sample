d1f9231009b15591ba6eb587e0912e45539e5eee
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-07-28 15:35:41 +0100

    change gitignore

---
+++ build/precache-manifest.ad4503b7b963b8f56e20d5d5342ac9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5fca30003e67fdc154355e67389cd8",
+    "url": "/kmughal/React-atomic-design-sample/index.html"
+  },
+  {
+    "revision": "34e655007659f642ccbf",
+    "url": "/kmughal/React-atomic-design-sample/static/css/2.677f2dd4.chunk.css"
+  },
+  {
+    "revision": "a7d119ff8a20b8ddbc59",
+    "url": "/kmughal/React-atomic-design-sample/static/css/main.46815887.chunk.css"
+  },
+  {
+    "revision": "34e655007659f642ccbf",
+    "url": "/kmughal/React-atomic-design-sample/static/js/2.7538a9f7.chunk.js"
+  },
+  {
+    "revision": "a7d119ff8a20b8ddbc59",
+    "url": "/kmughal/React-atomic-design-sample/static/js/main.ed7d9dd3.chunk.js"
+  },
+  {
+    "revision": "4dde1158cc5fd5bc0150",
+    "url": "/kmughal/React-atomic-design-sample/static/js/runtime~main.4e2a5a96.js"
+  }
+]);
