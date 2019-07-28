c537850f47e2e4cbbe75d1e596dbf3627e7c9ec3
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-07-28 16:45:56 +0100

    change

---
+++ build/precache-manifest.81de29fcaf33a267d24a556d3dc1338e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841da0a7d3bad2816b1751bca47b5148",
+    "url": "/react-atomic-design/index.html"
+  },
+  {
+    "revision": "34e655007659f642ccbf",
+    "url": "/react-atomic-design/static/css/2.677f2dd4.chunk.css"
+  },
+  {
+    "revision": "a7d119ff8a20b8ddbc59",
+    "url": "/react-atomic-design/static/css/main.46815887.chunk.css"
+  },
+  {
+    "revision": "34e655007659f642ccbf",
+    "url": "/react-atomic-design/static/js/2.7538a9f7.chunk.js"
+  },
+  {
+    "revision": "a7d119ff8a20b8ddbc59",
+    "url": "/react-atomic-design/static/js/main.ed7d9dd3.chunk.js"
+  },
+  {
+    "revision": "dcf92545bf20866f9b89",
+    "url": "/react-atomic-design/static/js/runtime~main.bae4fa50.js"
+  }
+]);
